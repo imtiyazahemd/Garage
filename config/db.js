dcfdf60d02a107403754c3660818fd81f6181081
imtiyazahmed <imtiyaz.n.ahmed@gmail.com>
2025-03-19 10:06:33 -0700

    swagger css

--- config/db.js
+++ config/db.js
@@ -6,10 +6,11 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+   
     
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
